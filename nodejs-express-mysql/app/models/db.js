9893e35489ffd0a365168ee0364f356c4f4b4f25
Hope Davis <s1072896@monmouth.edu>
2020-01-26 13:32:41 -0500

    db model

--- nodejs-express-mysql/app/models/db.js
+++ nodejs-express-mysql/app/models/db.js
@@ -11,8 +11,10 @@ const connection = mysql.createConnection({
 
 // open the MySQL connection
 connection.connect(error => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
+  if (error) {
+      throw error;
+  }
+  console.log('Successfully connected to the database.');
 });
 
 module.exports = connection;
